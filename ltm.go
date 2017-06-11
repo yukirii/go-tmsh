e8082bba0cabd66efe288a9febe38fd139011531
shiftky <shiftky@gmail.com>
2017-06-11 10:06:41 +0900

    Implement Create/Add security firewall policy functions

--- ltm.go
+++ ltm.go
@@ -220,3 +220,27 @@ func (bigip *BigIP) RouteAdvertisementEnabled(targetVIP string) error {
 	}
 	return nil
 }
+
+func (bigip *BigIP) CreateSecurityFWPolicy(firewallPolicyName string) error {
+	cmd := "create security firewall policy " + firewallPolicyName
+	ret, _ := bigip.ExecuteCommand(cmd)
+	if ret != "" {
+		return fmt.Errorf(ret)
+	}
+	return nil
+}
+
+func (bigip *BigIP) AddSecurityFWPolicyRule(firewallPolicyName, ruleName, action string, targetPort int) error {
+	if action != "accept" && action != "accept-decisively" &&
+		action != "drop" && action != "reject" {
+		return fmt.Errorf("Invalid action name: " + action)
+	}
+
+	cmd := "modify security firewall policy " + firewallPolicyName + " rules add { " + ruleName +
+		" { action " + action + " ip-protocol tcp destination { ports add { " + strconv.Itoa(targetPort) + " } } place-after last } }"
+	ret, _ := bigip.ExecuteCommand(cmd)
+	if ret != "" {
+		return fmt.Errorf(ret)
+	}
+	return nil
+}
