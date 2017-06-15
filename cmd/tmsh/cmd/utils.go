6f43af069364011270e36846841a72b4cd375352
shiftky <shiftky@gmail.com>
2017-06-16 00:12:12 +0900

    Implement node subcommands

---
+++ cmd/tmsh/cmd/utils.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/shiftky/go-tmsh"
+	"github.com/spf13/viper"
+)
+
+func NewSession() *tmsh.BigIP {
+	bigip, err := tmsh.NewSession(
+		viper.GetString("tmsh_host"),
+		viper.GetString("tmsh_port"),
+		viper.GetString("tmsh_user"),
+		viper.GetString("tmsh_password"),
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return bigip
+}
